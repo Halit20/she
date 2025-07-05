0071bc605252ebaf27faeae90400d1aed7f8821e
Halit20 <60716337+Halit20@users.noreply.github.com>
2025-07-05 22:58:51 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('LOVE U ELONA').split('')
+    const titles = ('LOVE U FIRDEVS').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
